aaeee9135c3a53eebe569f58227f0b1adf28c45d
nyrha23 <trinypasi@gmail.com>
2025-06-13 08:12:34 -0300

    Actualizaciones de estilo e imagenes

--- frontend/src/pages/register/register.jsx
+++ frontend/src/pages/register/register.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import axios from 'axios';
 import './register.css';
 import { Link, useNavigate } from 'react-router-dom';
-import { Form, Button, Row, Col } from 'react-bootstrap';
+import { Form, Button, Row, Col, Navbar, Container, Card, Alert, FormGroup, FormLabel, FormControl, CardBody } from 'react-bootstrap';
 
 const API = process.env.REACT_APP_API_URL;
 
@@ -51,76 +51,120 @@ const Registro = () => {
     };
 
     return (
-        <div className="register-wrapper">
-            <div className="register-container">
-                <h2 className="register-title">Registro</h2>
-                <Form onSubmit={handleSubmit} className="col-12">
-                    <Row className="form-row">
-                        <Col><Form.Control name="nombre" placeholder="Nombre" onChange={handleChange} required /></Col>
-                        <Col><Form.Control name="apellido" placeholder="Apellido" onChange={handleChange} required /></Col>
-                    </Row>
-
-                    <Row className="form-row">
-            
-                        <Col>
-                            <Form.Select name="tipodoc" onChange={handleChange}>
-                                <option value="DNI">DNI</option>
-                                <option value="Pasaporte">Pasaporte</option>
-                            </Form.Select>
-                        </Col>
-
-                        <Col>
-                            <Form.Control name="docum" placeholder="Nro. documento" onChange={handleChange} required />
-                        </Col>
-                        
-                    </Row>
-
-                    <Row className="form-row">
-                        <Col>
-                            <Form.Control type="date" name="fechanacim" onChange={handleChange} required />
-                        </Col>
-                        <Col>
-                            <Form.Control name="direccion" placeholder="Dirección" onChange={handleChange} />
-                        </Col>
-                    </Row>
-
-                    <Row className="form-row">
-                        <Col>
-                            <Form.Control name="telcontacto" placeholder="Tel. contacto" onChange={handleChange} />
-                        </Col>
-                        <Col>
-                            <Form.Control name="telemergencia" placeholder="Tel. emergencia" onChange={handleChange} />
-                        </Col>
-                    </Row>
-
-                    <Row className="form-row">
-                        <Col>
-                            <Form.Control type="email" name="correo" placeholder="Email" onChange={handleChange} required />
-                        </Col>
-                        <Col>
-                            <Form.Control type="email" name="confirmarCorreo" placeholder="Confirmar email" onChange={handleChange} required />
-                        </Col>
-                    </Row>
-
-                    <Row className="form-row">
-                        <Col>
-                            <Form.Control type="password" name="contrasena" placeholder="Contraseña" onChange={handleChange} required />
-                        </Col>
-                        <Col>
-                            <Form.Control type="password" name="confirmarContrasena" placeholder="Confirmar contraseña" onChange={handleChange} required />
-                        </Col>
-                    </Row>
-
-                    {error && <p className="error-message">{error}</p>}
-
-                    <Button type="submit" className="register-button">Registrar</Button>
-                    <Button variant="light" className="google-button d-none">Registrarse con Google</Button>
-                </Form>
-
-                <div className="register-link">
-                    ¿Ya tiene una cuenta? <Link to="/">Ingresar</Link>
-                </div>
+        <div className='d-flex min-vh-100 flex-column'>
+            <Navbar expand="lg" className="shadow bg-light px-4 py-2 justify-content-between">
+                <Navbar.Brand className="text-primary">
+                    <img src="logo.png" alt='logo' width={30} height={30} fluid className="d-inline-block align-top" />{' '}
+                    Clínica<b>Médica</b>
+                </Navbar.Brand>
+            </Navbar>
+            <div className="register-wrapper">
+                <Container>
+                    <Card className='p-4 shadow bg-white bg-opacity-75 border-0 rounded'>
+                        <CardBody>
+                            <h2 className="text-center mb-4">Registro</h2>
+                            <Form onSubmit={handleSubmit}>
+                                <Row className='mb-3'>
+                                    <FormGroup as={Col} md="6" className="mb-3" controlId="formNombre">
+                                        <FormLabel>Nombre</FormLabel>
+                                        <Form.Control name="nombre" placeholder="Nombre" onChange={handleChange} required />
+                                    </FormGroup>
+                                    <FormGroup as={Col} md="6" className="mb-3" controlId="formApellido">
+                                        <FormLabel>Apellido</FormLabel>
+                                        <Form.Control name="apellido" placeholder="Apellido" onChange={handleChange} required />
+                                    </FormGroup>
+                                </Row>
+                                <Row className='mb-3'>
+                                    <FormGroup as={Col} md="6" className="mb-3" controlId="formTipoDoc">
+                                        <FormLabel>Tipo de documento</FormLabel>
+                                        <Form.Select name="tipodoc" onChange={handleChange}>
+                                            <option value="DNI">DNI</option>
+                                            <option value="Pasaporte">Pasaporte</option>
+                                        </Form.Select>
+                                    </FormGroup>
+                                    <FormGroup as={Col} md="6" className='mb-3' controlId='formNumDoc'>
+                                        <FormLabel>Número de documento</FormLabel>
+                                        <FormControl name="docum" placeholder="Nro. documento" onChange={handleChange} required />
+                                    </FormGroup>
+                                </Row>
+                                <Row className='mb-3'>
+                                    <FormGroup as={Col} md="6" className='mb-3' controlId='formFechaNacim'>
+                                        <FormLabel>Fecha de nacimiento</FormLabel>
+                                        <FormControl type="date" name="fechanacim" onChange={handleChange} required />
+                                    </FormGroup>
+                                    <FormGroup as={Col} md="6" className='mb-3' controlId='formDireccion'>
+                                        <FormLabel>Dirección</FormLabel>
+                                        <FormControl name="direccion" placeholder="Dirección" onChange={handleChange} />
+                                    </FormGroup>
+                                </Row>
+                                <Row className='mb-3'>
+                                    <FormGroup as={Col} md="6" className="mb-3" controlId='formTelContacto'>
+                                        <FormLabel>Teléfono de contacto</FormLabel>
+                                        <FormControl name="telcontacto" placeholder="Tel. contacto" onChange={handleChange} />
+                                    </FormGroup>
+                                    <FormGroup as={Col} md="6" className="mb-3" controlId='formTelEmerg'>
+                                        <FormLabel>Teléfono de emergencia</FormLabel>
+                                        <FormControl name="telemergencia" placeholder="Tel. emergencia" onChange={handleChange} />
+                                    </FormGroup>
+                                </Row>
+                                <div className='d-flex justify-content-center align-selft-stretch border-1'></div>
+                                <Row className='mb-3'>
+                                    <FormGroup as={Col} md="6" className="mb-3" controlId='formEmail'>
+                                        <FormLabel>Email</FormLabel>
+                                        <FormControl type="email" name="correo" placeholder="Email" onChange={handleChange} required />
+                                    </FormGroup>
+                                    <FormGroup as={Col} md="6" className="mb-3" controlId='formConfEmail'>
+                                        <FormLabel>Confirmar email</FormLabel>
+                                        <FormControl type="email" name="confirmarCorreo" placeholder="Confirmar email" onChange={handleChange} required />
+                                    </FormGroup>
+                                </Row>
+                                <Row className='mb-3'>
+                                    <FormGroup as={Col} md="6" className="mb-3" controlId='formContrasenaReg'>
+                                        <FormLabel>Contraseña</FormLabel>
+                                        <FormControl type="password" name="contrasena" placeholder="Contraseña" onChange={handleChange} required />
+                                    </FormGroup>
+                                    <FormGroup as={Col} md="6" className="mb-3" controlId='formConfContrasenaReg'>
+                                        <FormLabel>Confirmar contraseña</FormLabel>
+                                        <FormControl type="password" name="confirmarContrasena" placeholder="Confirmar contraseña" onChange={handleChange} required />
+                                    </FormGroup>
+                                </Row>
+                                <Button type="submit" variant="primary" className="w-100">Registrar</Button>
+                            </Form>
+                            <div className="text-center mt-3">
+                                {error && <Alert variant="danger">{error}</Alert>}
+                            </div>
+                            <div className="text-center mt-3">
+                                ¿Ya tiene una cuenta?{' '} <Link to="/" className="text-decoration-none">Ingresar</Link>
+                            </div>
+                        </CardBody>
+                    </Card>
+                </Container>
             </div>
+            <footer className="text-bg-dark py-4 mt-auto">
+                <Container>
+                    <Row>
+                        <Col md={4}>
+                            <h5><img src="logo.png" alt='logo' width={30} height={30} fluid className="d-inline-block align-top" />{' '} Clínica<b>Médica</b></h5>
+                        </Col>
+                        <Col md={4}>
+                            <p><b>Información Institucional</b></p>
+                            <p>Especialidades médicas</p>
+                            <p>Calidad y seguridad del paciente</p>
+                        </Col>
+                        <Col md={4}>
+                            <p><b>Información Útil</b></p>
+                            <p>Coberturas médicas</p>
+                            <p>Solicite turno</p>
+                            <p>Preguntas frecuentes</p>
+                        </Col>
+                    </Row>
+                    <Row className="text-center mt-3">
+                        <Col>
+                            <small>©2025 Diseñado y desarrollado por <a className="text-info text-decoration-underline" href="https://www.linkedin.com/company/hifive-developers/" target="_blank" rel="noreferrer">HiFive Developers</a></small>
+                        </Col>
+                    </Row>
+                </Container>
+            </footer>
         </div>
     );
 };
