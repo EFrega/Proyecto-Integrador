331722b1ceb624ed3094d8b60029f0ddededc669
AleLoredo <loredozuleta.alejandro@gmail.com>
2025-06-14 21:36:15 -0300

    Register.jsx style fix

--- frontend/src/pages/register/register.jsx
+++ frontend/src/pages/register/register.jsx
@@ -63,72 +63,84 @@ const Registro = () => {
                 </Navbar.Brand>
             </Navbar>
             <div className="register-wrapper">
-                <Container>
-                    <Card className='p-4 shadow bg-white bg-opacity-75 border-0 rounded'>
+                <Container className="d-flex justify-content-center">
+                    <Card className='p-2 w-50 shadow bg-white bg-opacity-75 border-0 rounded mx-auto'>
                         <CardBody>
-                            <h2 className="text-center mb-4">Registro</h2>
+                            <h2 className="text-center mt-0">Registro</h2>
+                            <hr />
                             <Form onSubmit={handleSubmit}>
                                 <Row className='mb-3'>
                                     <FormGroup as={Col} md="6" className="mb-3" controlId="formNombre">
-                                        <FormLabel>Nombre</FormLabel>
+                                        {/* <FormLabel>Nombre</FormLabel> */}
                                         <Form.Control name="nombre" placeholder="Nombre" onChange={handleChange} required />
                                     </FormGroup>
                                     <FormGroup as={Col} md="6" className="mb-3" controlId="formApellido">
-                                        <FormLabel>Apellido</FormLabel>
+                                        {/* <FormLabel>Apellido</FormLabel> */}
                                         <Form.Control name="apellido" placeholder="Apellido" onChange={handleChange} required />
                                     </FormGroup>
                                 </Row>
                                 <Row className='mb-3'>
                                     <FormGroup as={Col} md="6" className="mb-3" controlId="formTipoDoc">
-                                        <FormLabel>Tipo de documento</FormLabel>
+                                        {/* <FormLabel>Tipo de documento</FormLabel> */}
                                         <Form.Select name="tipodoc" onChange={handleChange}>
                                             <option value="DNI">DNI</option>
                                             <option value="Pasaporte">Pasaporte</option>
                                         </Form.Select>
                                     </FormGroup>
                                     <FormGroup as={Col} md="6" className='mb-3' controlId='formNumDoc'>
-                                        <FormLabel>Número de documento</FormLabel>
+                                        {/* <FormLabel>Número de documento</FormLabel> */}
                                         <FormControl name="docum" placeholder="Nro. documento" onChange={handleChange} required />
                                     </FormGroup>
                                 </Row>
                                 <Row className='mb-3'>
                                     <FormGroup as={Col} md="6" className='mb-3' controlId='formFechaNacim'>
-                                        <FormLabel>Fecha de nacimiento</FormLabel>
-                                        <FormControl type="date" name="fechanacim" onChange={handleChange} required />
+                                        <FormControl 
+                                            type="text" 
+                                            name="fechanacim" 
+                                            placeholder="Fecha de nacimiento" 
+                                            onFocus={(e) => e.target.type = 'date'}
+                                            onBlur={(e) => !e.target.value && (e.target.type = 'text')}
+                                            onChange={handleChange} 
+                                            required 
+                                        />
                                     </FormGroup>
+                                    {/* <FormGroup as={Col} md="6" className='mb-3' controlId='formFechaNacim'>
+                                        <FormLabel>Fecha de nacimiento</FormLabel>
+                                        <FormControl type="date" name="fechanacim" placeholder="Fecha de nacimiento" onChange={handleChange} required />
+                                    </FormGroup> */}
                                     <FormGroup as={Col} md="6" className='mb-3' controlId='formDireccion'>
-                                        <FormLabel>Dirección</FormLabel>
+                                        {/* <FormLabel>Dirección</FormLabel> */}
                                         <FormControl name="direccion" placeholder="Dirección" onChange={handleChange} required />
                                     </FormGroup>
                                 </Row>
                                 <Row className='mb-3'>
                                     <FormGroup as={Col} md="6" className="mb-3" controlId='formTelContacto'>
-                                        <FormLabel>Teléfono de contacto</FormLabel>
+                                        {/* <FormLabel>Teléfono de contacto</FormLabel> */}
                                         <FormControl name="telcontacto" placeholder="Tel. contacto" onChange={handleChange} required />
                                     </FormGroup>
                                     <FormGroup as={Col} md="6" className="mb-3" controlId='formTelEmerg'>
-                                        <FormLabel>Teléfono de emergencia</FormLabel>
+                                        {/* <FormLabel>Teléfono de emergencia</FormLabel> */}
                                         <FormControl name="telemergencia" placeholder="Tel. emergencia" onChange={handleChange} />
                                     </FormGroup>
                                 </Row>
                                 <hr />
                                 <Row className='mb-3'>
                                     <FormGroup as={Col} md="6" className="mb-3" controlId='formEmail'>
-                                        <FormLabel>Email</FormLabel>
+                                        {/* <FormLabel>Email</FormLabel> */}
                                         <FormControl type="email" name="correo" placeholder="Email" onChange={handleChange} required />
                                     </FormGroup>
                                     <FormGroup as={Col} md="6" className="mb-3" controlId='formConfEmail'>
-                                        <FormLabel>Confirmar email</FormLabel>
+                                        {/* <FormLabel>Confirmar email</FormLabel> */}
                                         <FormControl type="email" name="confirmarCorreo" placeholder="Confirmar email" onChange={handleChange} required />
                                     </FormGroup>
                                 </Row>
                                 <Row className='mb-3'>
                                     <FormGroup as={Col} md="6" className="mb-3" controlId='formContrasenaReg'>
-                                        <FormLabel>Contraseña</FormLabel>
+                                        {/* <FormLabel>Contraseña</FormLabel> */}
                                         <FormControl type="password" name="contrasena" placeholder="Contraseña" onChange={handleChange} required />
                                     </FormGroup>
                                     <FormGroup as={Col} md="6" className="mb-3" controlId='formConfContrasenaReg'>
-                                        <FormLabel>Confirmar contraseña</FormLabel>
+                                        {/* <FormLabel>Confirmar contraseña</FormLabel> */}
                                         <FormControl type="password" name="confirmarContrasena" placeholder="Confirmar contraseña" onChange={handleChange} required />
                                     </FormGroup>
                                 </Row>
